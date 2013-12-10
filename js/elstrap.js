0fcdcd7767a7c81d5558cc0a8dd27b2424efcf0f
Abi أب <el@elabee.me>
2013-12-10 14:15:27 +0700

    Move preloader outside drupal behaviors

--- js/elstrap.js
+++ js/elstrap.js
@@ -1,18 +1,21 @@
 (function ($) {
 
+if (typeof($.easing.easeInExpo == 'undefined')) {
+  $.easing.easeInExpo = function (x, t, b, c, d) {
+    return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+  };
+};
+
+$(document).ready(function() {
+  $('#preloader').fadeTo(1000, 0.75, 'easeInExpo');
+});
+
+$(window).load(function() {
+  $('#preloader').fadeOut(1000, 'easeInExpo');
+});
+
 Drupal.behaviors.elstrap = {
   attach: function (context, settings) {
-    if (typeof($.easing.easeInExpo == 'undefined')) {
-      $.easing.easeInExpo = function (x, t, b, c, d) {
-        return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
-      };
-    };
-
-    $('#preloader').fadeTo(1000, 0.75, 'easeInExpo');
-
-    $(window).load(function() {
-      $('#preloader').fadeOut(1000, 'easeInExpo');
-    });
   }
 };
 
