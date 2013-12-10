e3c9b60812aa31a60c4bceaafd18d426c3a6b16a
Abi أب <el@elabee.me>
2013-12-10 19:17:22 +0700

    Use list-group

--- theme/pagespeed_insight/pagespeed-insight-stats.tpl.php
+++ theme/pagespeed_insight/pagespeed-insight-stats.tpl.php
@@ -6,38 +6,33 @@
       </a>
     </h4>
   </div>
-  <div id="statistic" class="panel-collapse collapse">
-    <div class="panel-body">
-      <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-4 text-center">
-          <canvas id="resources" width="250" height="150"></canvas>
-        </div>
-        <div class="col-xs-12 col-sm-6 col-md-8">
-          <ul>
-            <?php if (isset($stats['htmlResponseBytes'])): ?>
-              <li class="text-primary">HTML <span class="badge pull-right"><?php print format_size($stats['htmlResponseBytes']); ?></span></li>
-            <?php endif; ?>
-            <?php if (isset($stats['cssResponseBytes'])): ?>
-              <li class="text-success">CSS <span class="badge pull-right"><?php print format_size($stats['cssResponseBytes']); ?></span></li>
-            <?php endif; ?>
-            <?php if (isset($stats['imageResponseBytes'])): ?>
-              <li class="text-warning"><?php print t('Image'); ?> <span class="badge pull-right"><?php print format_size($stats['imageResponseBytes']); ?></span></li>
-            <?php endif; ?>
-            <?php if (isset($stats['javascriptResponseBytes'])): ?>
-              <li class="text-danger">JavaScript <span class="badge pull-right"><?php print format_size($stats['javascriptResponseBytes']); ?></span></li>
-            <?php endif; ?>
-            <?php if (isset($stats['flashResponseBytes'])): ?>
-              <li class="text-danger"><?php print t('JavaScript'); ?> <span class="badge pull-right"><?php print format_size($stats['flashResponseBytes']); ?></span></li>
-            <?php endif; ?>
-            <?php if (isset($stats['textResponseBytes'])): ?>
-              <li class="text-info"><?php print t('Text'); ?> <span class="badge pull-right"><?php print format_size($stats['textResponseBytes']); ?></span></li>
-            <?php endif; ?>
-            <?php if (isset($stats['otherResponseBytes'])): ?>
-              <li class="text-muted"><?php print t('Other'); ?> <span class="badge pull-right"><?php print format_size($stats['otherResponseBytes']); ?></span></li>
-            <?php endif; ?>
-          </ul>
-        </div>
-      </div>
+  <div id="statistic" class="panel panel-collapse collapse">
+    <div class="panel-body text-center">
+      <canvas id="resources"></canvas>
     </div>
+
+    <ul class="list-group">
+      <?php if (isset($stats['htmlResponseBytes'])): ?>
+        <li class="list-group-item text-primary"><strong>HTML</strong> <span class="badge"><?php print format_size($stats['htmlResponseBytes']); ?></span></li>
+      <?php endif; ?>
+      <?php if (isset($stats['cssResponseBytes'])): ?>
+        <li class="list-group-item text-success"><strong>CSS</strong> <span class="badge"><?php print format_size($stats['cssResponseBytes']); ?></span></li>
+      <?php endif; ?>
+      <?php if (isset($stats['imageResponseBytes'])): ?>
+        <li class="list-group-item text-warning"><strong><?php print t('Image'); ?></strong> <span class="badge"><?php print format_size($stats['imageResponseBytes']); ?></span></li>
+      <?php endif; ?>
+      <?php if (isset($stats['javascriptResponseBytes'])): ?>
+        <li class="list-group-item text-danger"><strong>JavaScript</strong> <span class="badge"><?php print format_size($stats['javascriptResponseBytes']); ?></span></li>
+      <?php endif; ?>
+      <?php if (isset($stats['flashResponseBytes'])): ?>
+        <li class="list-group-item text-danger"><strong><?php print t('JavaScript'); ?></strong> <span class="badge"><?php print format_size($stats['flashResponseBytes']); ?></span></li>
+      <?php endif; ?>
+      <?php if (isset($stats['textResponseBytes'])): ?>
+        <li class="list-group-item text-info"><strong><?php print t('Text'); ?></strong> <span class="badge"><?php print format_size($stats['textResponseBytes']); ?></span></li>
+      <?php endif; ?>
+      <?php if (isset($stats['otherResponseBytes'])): ?>
+        <li class="list-group-item text-muted"><strong><?php print t('Other'); ?></strong> <span class="badge"><?php print format_size($stats['otherResponseBytes']); ?></span></li>
+      <?php endif; ?>
+    </ul>
   </div>
 </div>
