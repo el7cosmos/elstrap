45391a90fbb435bb7154b1186502ac3edcdeede2
Abi أب <el@elabee.me>
2013-12-10 19:16:52 +0700

    Move pagespeed-insight.js to elstrap.js

--- js/elstrap.js
+++ js/elstrap.js
@@ -16,6 +16,17 @@ $(window).load(function() {
 
 Drupal.behaviors.elstrap = {
   attach: function (context, settings) {
+    var rendered = false;
+    $('#statistic').on('shown.bs.collapse', function () {
+      if (!rendered) {
+        var resources = $('#resources').get(0);
+        if (typeof resources !== 'undefined') {
+          resources = resources.getContext('2d');
+          var statistic = new Chart(resources).Pie(settings.pageSpeedInsight, { segmentShowStroke: false });
+          rendered = true;
+        };
+      };
+    })
   }
 };
 
