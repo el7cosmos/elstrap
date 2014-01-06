08d8bb628f5b58e10ad3e1b8af16b3f116eb6ed6
Abi أب <el@elabee.me>
2014-01-06 08:28:16 +0700

    Restructure HTML

---
+++ theme/block/block--adsense_managed.tpl.php
@@ -0,0 +1,15 @@
+<section id="<?php print str_replace('adsense', '', $block_html_id); ?>; ?>" class="<?php print str_replace('adsense', '', $classes); ?>"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <div class="panel-heading">
+      <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+    </div>
+  <?php endif;?>
+  <?php print render($title_suffix); ?>
+
+  <div class="panel-body">
+    <?php print $content ?>
+  </div>
+
+</section> <!-- /.block -->
